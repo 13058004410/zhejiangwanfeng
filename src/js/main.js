0e5e45457e7499be2d11404d56600abe4f355f97
scofield1906 <13058004410@163.com>
2020-10-06 10:15:13 +0800

    10.6

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,52 @@ $(document).ready(function(){
         $('.nav-m').toggle()
     })
 
+    // 电脑端当鼠标滑入导航栏li时出现下边框
+    $('#nav li').eq(0).mouseenter(
+        function(){
+            $(this).css('border-bottom','2px solid #1D6BC3')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('border-bottom','2px solid white')
+        }
+    )
+    $('#nav li').eq(1).mouseenter(
+        function(){
+            $(this).css('border-bottom','2px solid #1D6BC3')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('border-bottom','2px solid white')
+        }
+    )
+    $('#nav li').eq(2).mouseenter(
+        function(){
+            $(this).css('border-bottom','2px solid #1D6BC3')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('border-bottom','2px solid white')
+        }
+    )
+    $('#nav li').eq(3).mouseenter(
+        function(){
+            $(this).css('border-bottom','2px solid #1D6BC3')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('border-bottom','2px solid white')
+        }
+    )
+    $('#nav li').eq(4).mouseenter(
+        function(){
+            $(this).css('border-bottom','2px solid #1D6BC3')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('border-bottom','2px solid white')
+        }
+    )
 
     $(window).resize(function(){
           location.reload()
@@ -118,3 +164,108 @@ $(document).ready(function(){
         }
 })
 
+// 导航栏二级菜单JS代码
+$(function(){
+    // 走进万丰
+    $('#nav li').eq(0).mouseenter(
+        function(){
+            $('#m1').css('display','flex')
+        }).mouseleave(
+            function(){
+                    $('#m1').css('display','none')                   
+                })
+
+    $('#m1').mouseenter(
+        function(){
+            $(this).css('display','flex')
+        }    
+    ).mouseleave(
+        function(){
+                $('#m1').css('display','none')
+        })
+
+    // 新闻资讯
+    $('#nav li').eq(1).mouseenter(
+        function(){
+            $('#m2').css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $('#m2').css('display','none')
+        }
+    )
+    $('#m2').mouseenter(
+        function(){
+            $(this).css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('display','none')
+        })
+    
+    // 核心业务
+    $('#nav li').eq(2).mouseenter(
+        function(){
+            $('#m3').css('display','flex')
+        }).mouseleave(
+            function(){
+                $('#m3').css('display','none')
+            }
+        )
+    $('#m3').mouseenter(
+        function(){
+            $(this).css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('display','none')
+        }
+    ) 
+
+    // 媒体聚焦
+    $('#nav li').eq(3).mouseenter(
+        function(){
+            $('#m4').css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $('#m4').css('display','none')
+        }
+    )
+    $('#m4').mouseenter(
+        function(){
+            $(this).css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('display','none')
+        }
+    )
+
+    // 联系我们
+    $('#nav li').eq(4).mouseenter(
+        function(){
+            $('#m5').css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $('#m5').css('display','none')
+        }
+    )
+    $('#m5').mouseenter(
+        function(){
+            $(this).css('display','flex')
+        }
+    ).mouseleave(
+        function(){
+            $(this).css('display','none')
+        }
+    )
+
+ 
+
+
+
+
+})
+ 
