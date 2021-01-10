087f808621e66ef84b8fa88a2367962b2e0f8cf0
scofield <13058004410@163.com>
2021-01-10 22:38:12 +0800

    1.10_2

--- src/js/news.js
+++ src/js/news.js
@@ -158,7 +158,89 @@ $(document).ready(function(){
         }
     )    
 
-})
+
+    // 轮播图==================================
+    var imgCount=3;
+    var index=1;
+    var intervalId;
+    var buttonLi_m=$('.indecate_1').children();//htmlCollection集合
+
+    //自动轮播功能 使用定时器
+    autoNextPage();
+    function autoNextPage(){
+        // console.log('开始轮播')
+        intervalId=setInterval(function(){
+                nextPage(true);
+        },3000);
+    }
+
+    //当鼠标移入 停止轮播
+    $('.banner').mouseover(function(){
+        // console.log('鼠标移入，停止轮播');
+        clearInterval(intervalId);
+    });
+
+    //当鼠标移出 开始轮播
+    $('.banner').mouseout(function(){
+        // console.log('鼠标移出，开始轮播');
+        autoNextPage(true);
+    });
+
+    
+
+    
+    
+        var _width=$(window).width();
+        // console.log(_width);
+        clickButton();
+        function clickButton(){
+            var length=buttonLi_m.length;
+            for(var i=0;i<length;i++){
+                buttonLi_m[i].onclick=function(){
+                    $(buttonLi_m[index-1]).removeClass('active');
+                    if($(this).attr('index')==1){
+                        index=3;
+                    }else{
+                        index=$(this).attr('index')-1;
+                    }
+                        nextPage(true);
+                };
+            }
+        }
+
+        // 图片左右移动
+        function nextPage(next){
+            var imgWidth=$('.wrapper img').css('width');
+            var imgWidth_num=imgWidth.replace('px','');
+            var targetLeft=0;
+            //当前的圆点去掉active样式
+            $(buttonLi_m[index-1]).removeClass('active');
+            if(next){//往后走
+                if(index==3){//如果是最后一张 直接跳到第一张
+                    targetLeft=0;
+                    index=1;
+                    // console.log('自动轮播')
+                }else{
+                    index++;
+                    targetLeft=-imgWidth_num*(index-1);
+                }
+            }else{//往前走
+                if(index==1){//如果是在第一张 直接跳到第五张
+                    index=3;
+                    targetLeft=-imgWidth_num*(imgCount-1);
+                }else{
+                    index--;
+                    targetLeft=-imgWidth_num*(index-1);
+                }
+            }
+            $('.wrapper').animate({left:targetLeft+'px'});
+            //更新后的圆点加上样式
+            $(buttonLi_m[index-1]).addClass('active');
+        }
+    })
+
+
+
 
 
 
